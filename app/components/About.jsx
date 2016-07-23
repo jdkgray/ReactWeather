ca3b612287e1be2f0f5d1fc67020e440e3ae287d
Erik <egray@myclinicaltools.com>
2016-07-23 09:04:25 -0600

    Update About with Grid and Links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,19 +1,34 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-//var About = React.createClass({
-  //render: function () {
-    //return (
-      //<h3>About Component</h3>
-    //)
-  //}
-//});
-
-//Stateless functional components
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This weather app was created using the following resources:</p>
+      <ol>
+        <li>
+          <a href="http://openweathermap.org/api">Open Weather Map API</a>
+        </li>
+        <li>
+          <a href="https://github.com/">Git Hub</a>
+        </li>
+        <li>
+          <a href="https://www.heroku.com/home">Heroku</a>
+        </li>
+      </ol>
+      <p>I frequently check the weather for the following locations:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Pleasant Grove'>Pleasant Grove, UT</Link>
+        </li>
+        <li>
+          <Link to='/?location=Salem'>Salem, OR</Link>
+        </li>
+        <li>
+          <Link to='/?location=Lincoln City'>Lincoln City, OR</Link>
+        </li>
+      </ol>
     </div>
   )
 };
