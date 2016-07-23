b3eb2740fea876e3ce9b8ed866a84edd9491561e
Erik <egray@myclinicaltools.com>
2016-07-23 08:16:38 -0600

    Updated Main with Grid & Examples with sample locations

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,18 +1,18 @@
 var React = require('react');
-
-//var Examples = React.createClass({
-  //render: function () {
-    //return (
-      //<h3>Examples Component</h3>
-    //)
-  //}
-//});
-
+var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Pleasant Grove'>Pleasant Grove, UT</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 };
